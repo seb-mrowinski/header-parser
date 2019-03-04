7f78a456b62b884a329f2045b3b025117e808108
Seb Mrowinski <38937761+seb-mrowinski@users.noreply.github.com>
2019-03-04 11:56:16 +1100

    Update server.js

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/whoami", (req, res) => {
+app.get("api/whoami", (req, res) => {
   var ip_info = get_ip(req);
   res.json({"ipaddress": ip_info.clientIp, "language": req.headers['accept-language'], "software": req.headers['user-agent']});
 });
